bd4ac1b8beb3efc42fa8240b2ad2653097f47d61
Anekront <107873869+ASYarichuk@users.noreply.github.com>
2024-02-02 16:10:17 +0300

    Correct 1 comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Security.Cryptography;
 
 class Program
 {
@@ -36,33 +38,74 @@ public interface IPaymentSystem
     string GetPayingLink(Order order);
 }
 
-class PaySite : IPaymentSystem
+public interface IHashSystem
+{
+    string Hash(Order order);
+}
+
+class MD5Hesh : IHashSystem
+{
+    public string Hash(Order order)
+    {
+        string output;
+
+        MD5 MD5Hash = MD5.Create();
+
+        byte[] inputBytes = Encoding.ASCII.GetBytes(order.Id + order.Amount);
+
+        byte[] hash = MD5Hash.ComputeHash(inputBytes);
+
+        output = Convert.ToString(hash);
+
+        return output;
+    }
+}
+
+class SHA1Hesh : IHashSystem
+{
+    public string Hash(Order order)
+    {
+        string output;
+
+        SHA1 SHA1Hash = SHA1.Create();
+
+        byte[] inputBytes = Encoding.ASCII.GetBytes(order.Id + order.Amount);
+
+        byte[] hash = SHA1Hash.ComputeHash(inputBytes);
+
+        output = Convert.ToString(hash);
+
+        return output;
+    }
+}
+
+class PaySite : IPaymentSystem, MD5Hesh
 {
     private string _linkSite = "pay.system1.ru/order?amount=12000RUB&hash=";
 
     public string GetPayingLink(Order order)
     {
-        return $"{_linkSite}/MD5/{order.Id}";
+        return $"{_linkSite}{Hash(order)}{order.Id}";
     }
 }
 
-class OrderSite : IPaymentSystem
+class OrderSite : IPaymentSystem, MD5Hesh
 {
     private string _linkSite = "order.system2.ru/pay?hash=";
 
     public string GetPayingLink(Order order)
     {
-        return $"{_linkSite}/MD5/{order.Id.GetHashCode()}/{order.Amount}";
+        return $"{_linkSite}{Hash(order)}{order.Id.GetHashCode()}/{order.Amount}";
     }
 }
 
-class SystemSite : IPaymentSystem
+class SystemSite : IPaymentSystem, SHA1Hesh
 {
     private string _linkSite = "system3.com/pay?amount=12000&curency=RUB&hash=";
     private string _secretKey = "SecretKey";
 
     public string GetPayingLink(Order order)
     {
-        return $"{_linkSite}/SHA-1/{order.Id.GetHashCode()}/{order.Id}/{_secretKey}";
+        return $"{_linkSite}{Hash(order)}{order.Id.GetHashCode()}/{order.Id}/{_secretKey}";
     }
 }
